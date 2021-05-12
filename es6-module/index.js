19a2a83c97efd584211a20afe0f481fae2889db1
wxq01042524 <wxq01042524@alibaba-inc.com>
2021-05-12 15:29:55 +0800

    11

--- es6-module/index.js
+++
@@ -1,3 +0,0 @@
-require('babel-register');
-require('./import')
-require('./jiegou')
--- src/view/page/regular/encryption.vue
+++ src/view/page/regular/encryption.vue
@@ -7,8 +7,109 @@ export default {
   data() {
     return {};
   },
-  created() {},
-  methods: {}
+  created() {
+    //this.asyncAndAwait();
+    // this.promiseMethods();
+    // this.asyncAwaitPromise();
+    //this.promiseForMethods();
+    this.callbackMethods();
+  },
+  methods: {
+    promiseMethods() {
+      //问题例子  想让定时器拿到值  再执行下面操作
+      // let num;
+      // setTimeout(() => {
+      //   console.log("异步");
+      //   num = 1;
+      // }, 100);
+
+      // console.log("同步");
+      // console.log(num);
+
+      //解决方法
+      let num;
+      // const a = new Promise((res, reg) => {
+      //   setTimeout(() => {
+      //     num = 1;
+      //     res(num);
+      //   }, 100);
+      // });
+      // a.then((res) => {
+      //   console.log(res);
+      // });
+      new Promise((res, reg) => {
+        setTimeout(() => {
+          num = 1;
+          res(num);
+        }, 100);
+      }).then((res) => {
+        console.log(res);
+      });
+
+      //也可以解决  async 和await使用 比较简单
+    },
+    //async await promise结合使用
+    async asyncAwaitPromise() {
+      function printNum(arg) {
+        return new Promise((res) => {
+          setTimeout(() => {
+            res(arg);
+          }, 100);
+        });
+      }
+      const data = await printNum("测试");
+      console.log(data);
+      console.log("111111111111111111111111111111111111111111111");
+    },
+
+    //async await 处理循环异步的问题
+    asyncAndAwait() {
+      console.log("同步");
+      (async function (len) {
+        const newArr = [];
+        for (let i = 0; i < len; i++) {
+          await setTimeout(() => {
+            console.log(i + "异步变同步");
+          }, 100);
+        }
+        console.log(newArr);
+      })(10);
+    },
+    //promsie.all处理循环套异步的问题
+    promiseForMethods() {
+      function someFunction() {
+        let promises = [];
+        for (let i = 0; i < 10; i++) {
+          promises.push(
+            new Promise((res) => {
+              res(i);
+            })
+          );
+        }
+        return Promise.all(promises);
+      }
+
+      someFunction()
+        .then((results) => {
+          console.log(results);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    //回调函数处理异步问题
+    callbackMethods() {
+      function getData(callback) {
+        setTimeout(() => {
+          callback("callback处理异步");
+        }, 2000);
+      }
+      getData((val) => {
+        console.log(val);
+        console.log("callback");
+      });
+    },
+  },
 };
 </script>
 
@@ -36,8 +137,7 @@ export default {
 }
 .fade-enter,
     .fade-leave-to
-    /* .fade-leave-active below version 2.1.8 */
- {
+    /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
 </style>
